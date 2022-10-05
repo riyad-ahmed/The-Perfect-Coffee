3a2d9eec68f58f09b9b0d964fe23be6e03b6e84d
riyadahmed2809@gmail.com <riyadahmed2809@gmail.com>
2022-10-05 17:03:27 +0600

    js-prob

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 
-
-
-
-// window.onscroll = function() {myFunction()};
-
-// var header = document.getElementById("myHeader");
-// var sticky = header.offsetTop;
-
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
-
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.loge(entry)
+    console.log(entry)
     if (entry.isIntersecting){
         entry.target.classList.add('show');
     }
@@ -29,3 +13,6 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const tastElements = document.querySelectorAll('.tast');
+tastElements.forEach((el) => observer.observe(el));
