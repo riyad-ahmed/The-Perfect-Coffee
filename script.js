b83806fac02082994257c9367aac970323a558ad
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2022-10-11 00:22:00 +0600

    new-parallax_add

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const parallax = document.getElementById
+("parallax");
+
+window.addEventListener("scroll", function ()
+{
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset
+  * -0.7 + "px"
+})
+
+
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
